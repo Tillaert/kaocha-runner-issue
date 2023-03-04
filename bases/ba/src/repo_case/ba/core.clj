9a14b0f0565650a4e20a1f27b1c6a2fe639e51d9
Ardjan Dommisse <ardjan@gmail.com>
2023-03-04 12:09:07 +0100

    Added project and base

---
+++ bases/ba/src/repo_case/ba/core.clj
@@ -0,0 +1,1 @@
+(ns repo-case.ba.core)
---
+++ bases/ba/test/repo_case/ba/core_test.clj
@@ -0,0 +1,6 @@
+(ns repo-case.ba.core-test
+  (:require [clojure.test :as test :refer :all]
+            [repo-case.ba.core :as core]))
+
+(deftest dummy-test
+  (is (= 1 1)))
